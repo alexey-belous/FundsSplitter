a8391a0bb38a69b6837b400accf1f64881ab42f6
Alexey Belous <alexey.belous@sigma.software>
2020-06-05 12:49:21 +0300

    #18 Debts command handler implementation

---
+++ src/FundsSplitter.Core/Bot/Handlers/DebtsHandler.fs
@@ -0,0 +1,51 @@
+namespace FundsSplitter.Core.Bot.Handlers
+
+module DebtsHandler = 
+    open FundsSplitter.Core
+    open FundsSplitter.Core.Storage
+    open FundsSplitter.Core.Bot.Types
+    open FundsSplitter.Core.Bot.Message
+    open FundsSplitter.Core.Transactions.Types
+    open FundsSplitter.Core.Transactions.CrudOperations
+    open FundsSplitter.Core.Transactions.ProcessingLogic
+
+    open Telegram.Bot
+    open Telegram.Bot.Types
+
+    let ChatNotFoundError = "There's no Funds Splitter group in this chat."
+    let NoDebtsMessage = "All debts are splitted up!"
+    let DebtsAnswerRow userFrom userTo amount = 
+        sprintf "%s --> %s: %s" userFrom userTo (amount.ToString())
+
+    let handlerFunction botContext (update: Update) = 
+        fun () -> async {
+            let msg = update.Message
+            let client = botContext.BotClient
+            let db = botContext.Storage.Database
+            let chats = db.GetCollection(Collections.Chats)
+            let cts = botContext.CancellationToken
+
+            let tryFetchChat chatId = async {
+                let! chat = tryFindChat chats cts chatId
+                match chat with
+                | Some c -> return Ok c
+                | None -> return Error ChatNotFoundError
+            }
+
+            let formatDebts (debts: FundsSplitter.Core.Transactions.Types.Debt list) = 
+                if debts.Length = 0 then
+                    NoDebtsMessage
+                else
+                debts
+                |> List.map (fun d -> DebtsAnswerRow (formatUser d.From) (formatUser d.To) d.Amount)
+                |> String.concat "\n"
+
+            let! res = 
+                msg.Chat.Id
+                |> tryFetchChat
+                |> AsyncResult.map (createInitialDebtsMatrix >> getDebts)
+                |> AsyncResult.map formatDebts
+                |> Async.bind (sendMarkdownAnswer client msg cts)
+
+            return ()
+        } |> Some
--- src/FundsSplitter.Core/Bot/Message.fs
+++ src/FundsSplitter.Core/Bot/Message.fs
@@ -12,3 +12,20 @@ module Message =
 
     let extractCmd (message: Message) = 
         exctractEntitiesText MessageEntityType.BotCommand message |> Array.tryHead
+
+    let sendAnswerWithParseMode parseMode (client: TelegramBotClient) (msg: Message) cts res = 
+        let answer text = 
+            client.SendTextMessageAsync(new ChatId(msg.Chat.Id), text, parseMode, true, false, msg.MessageId, null, cts)
+            |> Async.AwaitTask
+
+        match res with
+        | Ok r -> answer r
+        | Error e -> answer e
+
+    let sendAnswer (client: TelegramBotClient) (msg: Message) cts res = 
+        sendAnswerWithParseMode (Enums.ParseMode.Default) client msg cts res
+    let sendMarkdownAnswer (client: TelegramBotClient) (msg: Message) cts res = 
+        sendAnswerWithParseMode (Enums.ParseMode.Markdown) client msg cts res
+
+    let formatUser (user: FundsSplitter.Core.Transactions.Types.User) = 
+        sprintf "%s (@%s)" (user.Name) (user.Username)
