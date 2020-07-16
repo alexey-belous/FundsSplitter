acdbdb97d15994c72cca575e31ee837e66a2f108
Alexey <what.if.do.best@gmail.com>
2020-07-16 09:54:44 +0300

    Pre-release FS 0.2 (#34)
    * Trigger build
    
    * #32 Handling of the /start command (#33)
    
    * #32 Handling of the /start command
    
    * Added feedback link

--- src/FundsSplitter.Core/Bot/Handlers/HelpHandler.fs
+++ src/FundsSplitter.Core/Bot/Handlers/HelpHandler.fs
@@ -11,7 +11,8 @@ module HelpHandler =
     [<Literal>]
     let helpMessageEn = """
        Funds Splitter v1.0
-To start work with this bot each user must send /join command to joint the group.
+To start work with this bot, it should be added to the Telegram chat.
+Then, each user must send /join command to joint the group.
 
 - `/help` - Shows a description of commands and how bot works.
 - `/join` - Joins user to a **splitting group**.
@@ -23,22 +24,27 @@ Examples:
 
 - `/payback @user1 100` - Add payback of a user (`/payback @user1 10` - @user1 gave 10 UAH to the author of the message).
 - `/debts` - Shows all debts.
+
+If you found a bug or have a suggestion, feel free to write it here: https://github.com/alexey-belous/FundsSplitter/issues.
     """
 
     let helpMessageRu = """
        Funds Splitter v1.0
-Для начала пользования этим ботом, все учасники должны выполнить комманду /join что б присоедениться к группе.
+Для начала пользования этим ботом, он должен быть добавлен в Telegram чат.
+После чего все учасники должны выполнить комманду /join что б присоедениться к группе.
 
 - `/help` - показывает описание команд и то, как работает бот.
 - `/join` - Добавляет пользователя к группе.
-- `/pay 100` - Добавляет платеж пользователя, отправившего сообщение 
 
+- `/pay 100` - Добавляет платеж пользователя, отправившего сообщение 
 Примеры: 
--- `/pay 10 описание (необязательное)` добавляет 10 грн и разделяет их между всеми участниками группы.
+-- `/pay 10 описание (необязательное)...` добавляет 10 грн и разделяет их между всеми участниками группы.
 -- `/pay 10 @user1 @user2 описание (необязательное)...` добавляет 10 грн и разделяет их между пользователями @user1 и @user2.
 
 - `/payback @user1 100` - Добавляет возврат средств пользователя в группу (`/payback @user1 10` - @user1 вернул 10 грн автору сообщения).
 - `/debts` - Показывает все долги в группе.
+
+Если у вы нашли баг или у вас есть предложение по улучшению, можете написать это здесь: https://github.com/alexey-belous/FundsSplitter/issues.
     """
 
     let helpMessage (lang: LanguageCode) = 
